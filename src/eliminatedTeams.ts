b75d85b15d83c5b7576d4048639340ddc30d95b6
Nick Dubelman <ndubelman@gmail.com>
2025-04-15 19:22:27 -0700

    reset eliminatedTeams

--- src/eliminatedTeams.ts
+++ src/eliminatedTeams.ts
@@ -1,25 +1,4 @@
 // Hardcoded list of eliminated teams
-const eliminatedTeams: string[] = [
-  'GSW',
-  'ATL',
-  'CHI',
-  'SAC',
-  'PHX',
-  'NOP',
-  'LAL',
-  'MIA',
-  'MIL',
-  'PHI',
-  'LAC',
-  'ORL',
-  'CLE',
-  'OKC',
-  'NYK',
-  'DEN',
-  'IND',
-  'MIN',
-  'DAL',
-  'BOS',
-]
+const eliminatedTeams: string[] = []
 
 export default eliminatedTeams
